a8e1c580bcff02932bc72069e020332ccb49d5ec
Tanush08 <71077849+Tanush08@users.noreply.github.com>
2020-09-18 19:40:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,16 +15,13 @@ function setup() {
 
 function draw() {
   background(0,0,0);  
-
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
-    movingRect.velocityX = movingRect.velocityX * (-1);
-    fixedRect.velocityX = fixedRect.velocityX * (-1);
-  }
-  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
-    movingRect.velocityY = movingRect.velocityY * (-1);
-    fixedRect.velocityY = fixedRect.velocityY * (-1);
-  }
+bounceOff(movingRect,fixedRect)
+if(isTouching(movingRect,fixedRect)){ 
+  movingRect.shapeColor = "blue"; 
+  fixedRect.shapeColor = "blue"; 
+} 
+  else { movingRect.shapeColor = "green"; 
+  fixedRect.shapeColor = "green"; 
+}
   drawSprites();
 }
